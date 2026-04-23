ad264694ad8c5517bc4087d3565e767e950a1a81
Abhijeet Farane <afarane@gmail.com>
2026-04-23 20:16:35 +0530

    Updated List

---
+++ 01_Basics/01_variables.ts
@@ -0,0 +1,14 @@
+// Variables in TypeScript
+let userName: string = "Abhijeet";
+let userAge: number = 30;
+let isAdmin: boolean = true;
+
+// Using 'let' allows us to reassign values
+userName = "John";
+userAge = 25;
+isAdmin = false;   
+
+// Using 'const' for variables that won't be reassigned
+const pi: number = 3.14;
+// pi = 3.14159; // This will cause an error because 'pi' is a constant 
+
